ebebfae99933c5b84eaf10a6e7a11edc76a59030
Fatih Demir <fatihdemirtc@gmail.com>
2025-10-19 18:17:00 +0300

    Implement user registration and login functionality with Angular components and services

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,25 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { lastValueFrom } from 'rxjs';
+import { Home } from './features/home/home';
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
 
-  private http=inject(HttpClient);
+  private accountService = inject(AccountService);
+  private http = inject(HttpClient);
 
-  protected  title = 'Client';
-  protected members=signal<any>([]);
+  protected title = 'Client';
+  protected members = signal<User[]>([]);
 
-  ngOnInit(): void {
-   this.http.get('https://localhost:5001/api/members').subscribe({
-    next:response=> this.members.set(response),
-    error:error=>console.error(),
-    complete:()=>console.log('Completed http request')
-   })
+
+  async ngOnInit() {
+    this.setCurrentUser();
+    this.members.set(await this.getMembers());
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+      return [];
+    }
   }
 }
