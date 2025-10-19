e30f8f99d63b7db09cb479a0aa1d43cda4a52129
Fatih Demir <fatihdemirtc@gmail.com>
2025-10-20 02:04:14 +0300

    Add routing, guards, and initialization logic; implement toast notifications

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,47 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "./layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { lastValueFrom } from 'rxjs';
-import { Home } from './features/home/home';
-import { User } from '../types/user';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+export class App {
+  protected router = inject(Router);
 
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-
-  protected title = 'Client';
-  protected members = signal<User[]>([]);
-
-
-  async ngOnInit() {
-    this.setCurrentUser();
-    this.members.set(await this.getMembers());
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-      return [];
-    }
-  }
 }
